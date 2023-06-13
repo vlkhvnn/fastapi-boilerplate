ee9ae54553a6a89156a7e55bd7fc2db43b8747f0
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-06-13 17:47:53 +0600

    changes

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.auth.router import router as auth_router
 from app.config import client, env, fastapi_config
-
 app = FastAPI(**fastapi_config)
 
 
