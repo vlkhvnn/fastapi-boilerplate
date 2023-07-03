fae7c468daeb9e633289dd052f5ef7a003d5b674
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-07-03 11:43:08 +0600

    reset auth

--- app/auth/router/router_authorize_user.py
+++ app/auth/router/router_authorize_user.py
@@ -9,11 +9,6 @@ from . import router
 from .errors import InvalidCredentialsException
 
 
-class RegisterUserRequest(AppModel):
-    email: str
-    password: str
-
-
 class AuthorizeUserResponse(AppModel):
     access_token: str
     token_type: str = "Bearer"
@@ -21,10 +16,10 @@ class AuthorizeUserResponse(AppModel):
 
 @router.post("/users/tokens", response_model=AuthorizeUserResponse)
 def authorize_user(
-    input: RegisterUserRequest,
+    input: OAuth2PasswordRequestForm = Depends(),
     svc: Service = Depends(get_service),
 ) -> AuthorizeUserResponse:
-    user = svc.repository.get_user_by_email(input.email)
+    user = svc.repository.get_user_by_email(input.username)
 
     if not user:
         raise InvalidCredentialsException
