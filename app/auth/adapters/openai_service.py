2592a4e324248b100f872607f330c7c49a2d071c
Alikhan Tangirbergen <tanir.alikh@gmail.com>
2023-07-11 15:15:33 +0600

    without token

--- app/auth/adapters/openai_service.py
+++ app/auth/adapters/openai_service.py
@@ -6,8 +6,7 @@ openai.api_key = openai_api
 class OpenAIService:
     def generate_chat_response(self, user_message, chat_history) -> str:
         # OpenAI API request
-        chat_history.append({"role": "user", "content": f"{user_message}"})
-        conversation_history = "\n".join(f"{message['role']}: {message['content']}" for message in chat_history)
+        conversation_history = chat_history + "\n" + user_message
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -29,6 +28,5 @@ class OpenAIService:
             temperature=0,
         )
         response = completion.choices[0].message["content"]
-        chat_history.append({"role": "assistant", "content": f"{response}"})
         # Extracting the generated response from OpenAI
         return response
--- app/auth/router/router_get_chat_responce.py
+++ app/auth/router/router_get_chat_responce.py
@@ -1,19 +1,13 @@
 from . import router
 from fastapi import Depends
 from app.auth.service import Service, get_service
-from ..adapters.jwt_service import JWTData
-from .dependencies import parse_jwt_user_data
 
 
 @router.get("/chat/getresponse")
 def get_response(
+    chat_history: str,
     question: str,
     svc: Service = Depends(get_service),
-    jwt_data: JWTData = Depends(parse_jwt_user_data),
 ) -> str:
-    svc.repository.add_user_message(jwt_data.user_id, question)
-    chat_history = svc.repository.get_chat_history_by_id(jwt_data.user_id)
-    token_ensured_chat = chat_history[-10:]
-    response = svc.openaiService.generate_chat_response(question, token_ensured_chat)
-    svc.repository.add_response(jwt_data.user_id, response)
+    response = svc.openaiService.generate_chat_response(question, chat_history)
     return response
